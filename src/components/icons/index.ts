a96f24a3923bef9bcd83fb8bbde76898f8b2099e
anhtcogn <anhtcogn@gmail.com>
2025-02-27 22:48:56 +0700

    fix icon

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -3,6 +3,6 @@ export { default as ArrowDownIcon } from '@/components/icons/ArrowDown.svg';
 export { default as ArrowUpIcon } from '@/components/icons/ArrowUpIcon.svg';
 export { default as DeleteIcon } from '@/components/icons/DeleteIcon.svg';
 export { default as UserIcon } from '@/components/icons/UserIcon.svg';
-export { default as Eye } from '@/components/icons/eye.svg';
+export { default as Eye } from '@/components/icons/Eye.svg';
 export { default as Calendar } from '@/components/icons/Calendar.svg';
 export { default as Comment } from '@/components/icons/Comment.svg';
