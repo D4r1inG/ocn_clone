b996506aaae70349c53b33fb69ea9b8d0a6bf946
anhtcogn <anhtcogn@gmail.com>
2025-03-15 16:40:16 +0700

    update service section

--- src/components/services/index.tsx
+++ src/components/services/index.tsx
@@ -1,58 +1,20 @@
-import React, { SVGProps } from 'react';
+import React from 'react';
 import { useTranslation } from 'next-i18next';
-import { Cursor } from '../common/Cursor';
 import { Boxes, HardDrive, Lock, Monitor, ScanEye } from 'lucide-react';
-
-const MouseIcon = (props: SVGProps<SVGSVGElement>) => {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" width={26} height={31} fill="none" {...props}>
-      <g clipPath="url(#a)">
-        <path
-          fill={'#c4c4c4'}
-          fillRule="evenodd"
-          stroke={'#fff'}
-          strokeLinecap="square"
-          strokeWidth={2}
-          d="M21.993 14.425 2.549 2.935l4.444 23.108 4.653-10.002z"
-          clipRule="evenodd"
-        />
-      </g>
-      <defs>
-        <clipPath id="a">
-          <path fill={'#22c55e'} d="M0 0h26v31H0z" />
-        </clipPath>
-      </defs>
-    </svg>
-  );
-};
+import { motion } from 'framer-motion';
 
 const FeatureCard = ({ icon, title, description }) => {
   return (
-    <div className="feature-card is-bordered has-text-centered is-feature-reveal">
-      <Cursor
-        attachToParent
-        variants={{
-          initial: { scale: 0.3, opacity: 0 },
-          animate: { scale: 1, opacity: 1 },
-          exit: { scale: 0.3, opacity: 0 },
-        }}
-        transition={{
-          ease: 'easeInOut',
-          duration: 0.15,
-        }}
-        className="left-12 top-4"
-      >
-        <MouseIcon className="h-6 w-6" />
-
-        <div className="ml-4 mt-1 rounded-[4px] bg-gray-100 p-4 text-gray-700 max-w-[300px] shadow-md">
-          {description}
-        </div>
-      </Cursor>
-      <div className="card-title">
-        <h4>{title}</h4>
+    <motion.div
+      whileHover={{ y: -4, borderColor: 'transparent', boxShadow: '0 20px 15px -12px rgba(0, 0, 0, 0.25)' }}
+      className="is-bordered border border-gray-100 border-solid p-6 rounded-2xl"
+    >
+      <div className="card-icon text-black mb-4">{icon}</div>
+      <h4 className="font-bold text-2xl w-fit whitespace-nowrap">{title}</h4>
+      <div className="line-clamp-2 text-gray-400" title={description}>
+        {description}
       </div>
-      <div className="card-icon">{icon}</div>
-    </div>
+    </motion.div>
   );
 };
 
@@ -61,27 +23,27 @@ export const Services = () => {
 
   const FEATURES = [
     {
-      icon: <HardDrive />,
+      icon: <HardDrive width={32} height={32} />,
       title: t('feat.cloud.title'),
       description: t('feat.cloud.description'),
     },
     {
-      icon: <Lock />,
+      icon: <Lock width={32} height={32} />,
       title: t('feat.firewall.title'),
       description: t('feat.firewall.description'),
     },
     {
-      icon: <ScanEye />,
+      icon: <ScanEye width={32} height={32} />,
       title: t('feat.consult.title'),
       description: t('feat.consult.description'),
     },
     {
-      icon: <Monitor />,
+      icon: <Monitor width={32} height={32} />,
       title: t('feat.service.title'),
       description: t('feat.service.description'),
     },
     {
-      icon: <Boxes />,
+      icon: <Boxes width={32} height={32} />,
       title: t('feat.rescue.title'),
       description: t('feat.rescue.description'),
     },
@@ -111,7 +73,7 @@ export const Services = () => {
           </div>
           <div className="columns justify-center">
             {FEATURES.slice(3).map((feature, index) => (
-              <div className="column is-4" key={index}>
+              <div className="column is-6" key={index}>
                 <FeatureCard {...feature} />
               </div>
             ))}
